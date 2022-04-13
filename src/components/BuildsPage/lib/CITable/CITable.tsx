d9b9deacf7c6a25d4d4ced881e65c7901249071f
Nima Kaviani <nkaviani@amazon.com>
2022-04-13 13:36:49 -0700

    more cleanup

--- src/components/BuildsPage/lib/CITable/CITable.tsx
+++ src/components/BuildsPage/lib/CITable/CITable.tsx
@@ -14,13 +14,10 @@
  * limitations under the License.
  */
 import React from 'react';
-import { Box, IconButton, Typography} from '@material-ui/core';
-import { Modal } from '@material-ui/core';
+import { Box, Typography} from '@material-ui/core';
+import { Link } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import RetryIcon from '@material-ui/icons/Replay';
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import CloseIcon from '@material-ui/icons/Close';
-/* import { generatePath, Link as RouterLink } from 'react-router-dom'; */
 import { RunStatus } from '../Status';
 import { getBuilds } from '../../../useBuilds';
 import { getDeployments } from '../../../useBuilds';
@@ -48,27 +45,18 @@ const generatedColumns: TableColumn[] = [
     highlight: true,
     render: (row: Partial<Build>) => {
       if (!row.id || !row.projectName) {
+        const arn = row.arn?.split(':');
+        if (arn == undefined || arn.length < 7) {
+          return (<> {row.id} </>)
+        }
         return (
-          <>
-            {row.id ||
-              row.projectName ||
-              'Unknown'}
-          </>
+          <Link href= {"https://"+arn[3]+".console.aws.amazon.com/codesuite/codebuild/"+arn[4]+"/"+arn[5].replace('build','projects')+"/"+arn[5]+":"+arn[6]} target="_blank" >
+            {row.id}
+          </Link>
         );
+      } else {
+        return (<> {row.id} </>);
       }
-
-      return (
-        row.id
-        /* <Link */
-        /*   component={RouterLink} */
-        /*   to={generatePath(buildRouteRef.path, { */
-        /*     jobFullName: encodeURIComponent(row.id), */
-        /*     buildNumber: String(row.buildNumber), */
-        /*   })} */
-        /* > */
-          /* {row.id} */
-        /* </Link> */
-      );
     },
   },
   {
@@ -108,39 +96,6 @@ const generatedColumns: TableColumn[] = [
       }
     },
   },
-  /* { */
-  /*   title: 'Failure Diagnosis', */
-  /*   render: (row: Partial<BatchGetBuildsOutput>) => ( */
-  /*     <> */
-  /*       <p>{getDiagnosis(row)}</p> */
-  /*     </> */
-  /*   ), */
-  /* }, */
-  /* { */
-  /*   title: 'Error', */
-  /*   field: 'lastBuild.source.branchName', */
-  /*   render: (row: Partial<BatchGetBuildsOutput>) => { */
-  /*       var exception = getError(row); */
-  /*       if (!exception) */
-  /*         return (<></>) */
-
-  /*       return ( */
-  /*         simpleModal(exception.exceptionType, exception.details.errors[0]) */
-  /*       ); */
-  /*   }, */
-  /* }, */
-  /* { */
-  /*   title: 'Actions', */
-  /*   sorting: false, */
-  /*   render: (row: Partial<Build>) => ( */
-  /*     <Tooltip title="Rerun pipeline"> */
-  /*       <IconButton onClick={row.onRestartClick}> */
-  /*         <RetryIcon /> */
-  /*       </IconButton> */
-  /*     </Tooltip> */
-  /*   ), */
-  /*   width: '10%', */
-  /* }, */
 ];
 
 type Props = {
@@ -200,135 +155,3 @@ export const CITable = () => {
     </>
   );
 };
-
-/* export function getDiagnosis(p : Partial<BatchGetBuildsOutput>) { */
-/*   if (!p) */
-/*     return "invalid build"; */
-
-/*   if (p.status && p.status.toLowerCase() == "success") { */
-/*     return "all good"; */
-/*   } */
-
-/*   var ref : string = "" */
-/*   if (!p.stages) */
-/*     return "invalid stages"; */
-
-/*   p.stages.forEach((s : Stage) => { */
-/*       if (s.status.toLowerCase() != "terminal") { */
-/*         return; */
-/*       } */
-/*       ref += s.name; */
-/*       s.tasks.forEach((t: Task) => { */
-/*         if (t.status.toLowerCase() != "terminal") { */
-/*           return; */
-/*         } */
-/*         ref += " > " + t.name; */
-/*       }) */
-/*   }) */
-/*   return ref */
-/* } */
-
-/* export function getError(p : Partial<BatchGetBuildsOutput>) : (Exception  | undefined) { */
-/*   if (!p || !p.stages || !p.status || p.status.toLowerCase() != "terminal") */
-/*     return undefined; */
-
-/*   let details : Exception | undefined = undefined; */
-/*   p.stages.forEach((s : Stage) => { */
-/*       if (s.status.toLowerCase() != "terminal") { */
-/*         return; */
-/*       } */
-
-/*       if (s.outputs && s.outputs.failureMessage) { */
-/*         details = { */
-/*           exceptionType: "Failed Output", */
-/*           details: { */
-/*             stackTrace: "", */
-/*             error: "Failed Output", */
-/*             errors: [s.outputs.failureMessage] */
-/*           } */
-/*         } */
-/*         return; */
-/*       } */
-
-/*       if (!s.context || */
-/*           !s.context.exception || */
-/*           !s.context.exception.details || */
-/*           !s.context.exception.details.errors) { */
-/*           return */
-/*       } */
-
-/*       if (!s.context.exception.details || s.context.exception.details.errors.length < 1) */
-/*         return; */
-
-/*       details = s.context.exception; */
-/*       return; */
-/*   }) */
-/*   return details */
-/* } */
-
-export function simpleModal(title: string, content: string) {
-      const [open, setOpen] = React.useState(false);
-
-      const handleOpen = () => {
-        setOpen(true);
-      };
-
-      function getModalStyle() {
-        const top = 50;
-        const left = 50;
-
-        return {
-          top: `${top}%`,
-          left: `${left}%`,
-          transform: `translate(-${top}%, -${left}%)`,
-          'overflow-wrap': 'break-word',
-          'overflow-y': 'auto',
-          border: '1px solid #000',
-          padding: '3px',
-          'background-color': 'rgba(250, 250, 230, 0.8)',
-        };
-      }
-
-      const [modalStyle] = React.useState(getModalStyle);
-
-      const handleClose = () => {
-        setOpen(false);
-      };
-
-    const body = (
-      <p>
-          <Grid container direction="row" alignItems="center">
-            <Grid item>
-              <IconButton onClick={handleClose}>
-                <CloseIcon />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <h2>{title}</h2>
-            </Grid>
-          </Grid>
-          <p>{content}</p>
-      </p>
-    );
-
-      return (
-      <div>
-        <Grid container direction="row" alignItems="center">
-          <Grid item>
-            <p>See Details</p>
-          </Grid>
-          <Grid item>
-            <IconButton onClick={handleOpen}>
-              <KeyboardArrowRightIcon/>
-            </IconButton>
-          </Grid>
-        </Grid>
-        <Modal
-          style={modalStyle}
-          open={open}
-          onClose={handleClose}
-        >
-          {body}
-        </Modal>
-      </div>);
-}
